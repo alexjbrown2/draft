2058061186985b1185b699c87dc8ae14d1e89e56
root <root@first-app.localdomain>
2019-01-03 16:42:13 +0000

    deleting everything but the data

--- data-analysis.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    var rankingsppr;
-    var rankingsstd;
-    $.getJSON('rankingsppr.json', function(data){
-        rankingsppr = data;
-    })
-    console.log(rankingsppr);
-
-})
